3a5848dd029662219a00d45f6946134f5710b58e
Claude Bing <me@claudebing.com>
2015-09-26 05:40:25 -0400

    Add warning for sending user messages

--- includes/modules/admin/user/message/send.inc.php
+++ includes/modules/admin/user/message/send.inc.php
@@ -2,31 +2,39 @@
 if(isset($_POST['messageBody'])){
     $messageSender      = $_SESSION['userEmail'];
     $messageRecipient   = $objUser->getUserEmail();
-    $messageSubject     = $_POST['messageSubject'];
+    $messageSubject     = "New Message from " . $user->getFullName() . " at CDCMastery.com";
     $messageQueueUser   = $_SESSION['userUUID'];
     $messageBody        = $_POST['messageBody'];
 
-    $emailBodyHTML	= "<html><head><title>".$messageSubject."</title></head><body>";
+    $emailBodyHTML	= "<html><head><title>New Message from " . $user->getFullName() . " at CDCMastery.com</title></head><body>";
     $emailBodyHTML .= $objUser->getFullName().",";
+    $emailBodyHTML .= "<br /><br />";
+    $emailBodyHTML .= "You have a new message from a manager at CDCMastery.com:";
     $emailBodyHTML .= "<br /><br />";
     $emailBodyHTML .= nl2br($messageBody,true);
     $emailBodyHTML .= "<br /><br />";
     $emailBodyHTML .= "Regards,";
     $emailBodyHTML .= "<br /><br />";
     $emailBodyHTML .= $user->getUserNameByUUID($_SESSION['userUUID']);
+    $emailBodyHTML .= "<br /><br />";
+    $emailBodyHTML .= "<i>Note: If you do not know the person who sent this message, or the contents are unprofessional, please forward this e-mail to admin@cdcmastery.com</i>";
     $emailBodyHTML .= "</body></html>";
 
     $emailBodyText = $objUser->getFullName().",";
+    $emailBodyText .= "\r\n\r\n";
+    $emailBodyText .= "You have a new message from a manager at CDCMastery.com:";
     $emailBodyText .= "\r\n\r\n";
     $emailBodyText .= $messageBody;
     $emailBodyText .= "\r\n\r\n";
     $emailBodyText .= "Regards,";
     $emailBodyText .= "\r\n\r\n";
     $emailBodyText .= $user->getUserNameByUUID($_SESSION['userUUID']);
+    $emailBodyText .= "\r\n\r\n";
+    $emailBodyText .= "Note: If you do not know the person who sent this message, or the contents are unprofessional, please forward this e-mail to admin@cdcmastery.com";
 
     if(!$emailQueue->queueEmail($messageSender,$messageRecipient,$messageSubject,$emailBodyHTML,$emailBodyText,$messageQueueUser)){
         $log->setAction("ERROR_EMAIL_QUEUE_ADD");
-        $log->setDetail("Calling Script","user/<user uuid>/message/send");
+        $log->setDetail("Calling Script","user/".$objUser->getUUID()."/message/send");
         $log->setDetail("Message Sender",$messageSender);
         $log->setDetail("Message Recipient",$messageRecipient);
         $log->setDetail("Message Subject",$messageSubject);
@@ -36,6 +44,16 @@ if(isset($_POST['messageBody'])){
         $log->saveEntry();
     }
     else{
+        $log->setAction("SEND_USER_MESSAGE");
+        $log->setDetail("Calling Script","user/".$objUser->getUUID()."/message/send");
+        $log->setDetail("Message Sender",$messageSender);
+        $log->setDetail("Message Recipient",$messageRecipient);
+        $log->setDetail("Message Subject",$messageSubject);
+        $log->setDetail("HTML Body",$emailBodyHTML);
+        $log->setDetail("Text Body",$emailBodyText);
+        $log->setDetail("Queue User",$messageQueueUser);
+        $log->saveEntry();
+
         $sysMsg->addMessage("Message queued for delivery.");
     }
 }
@@ -55,7 +73,7 @@ if(isset($_POST['messageBody'])){
                         <li><a href="/admin/users/<?php echo $userUUID; ?>"><i class="icon-inline icon-20 ic-arrow-left"></i>Return to user manager</a></li>
                     </ul>
                 </div>
-                <div class="clearfix"><br></div>
+                <div class="clearfix">&nbsp;</div>
             </section>
         </div>
         <div class="6u">
@@ -63,17 +81,22 @@ if(isset($_POST['messageBody'])){
                 <header>
                     <h2>Message Form</h2>
                 </header>
+                <p>
+                    Please be professional and courteous when sending messages to users, <strong>especially if they do not know you</strong>.
+                    All messages are logged for security purposes, and the user is instructed to forward all questionable messages to the site
+                    administrator.
+                </p>
                 <form action="/admin/users/<?php echo $userUUID; ?>/message/send" method="POST">
                     <ul>
                         <li>
                             <label for="messageSubject">Subject</label>
                             <br>
-                            <input type="text" id="messageSubject" name="messageSubject">
+                            <input class="input_full" type="text" id="messageSubject" name="messageSubject" value="New Message from <?php echo $user->getFullName(); ?> at CDCMastery.com" DISABLED="true">
                         </li>
                         <li>
                             <label for="messageBody">Body</label>
                             <br>
-                            <textarea id="messageBody" name="messageBody" style="height:6em;"></textarea>
+                            <textarea class="input_full" id="messageBody" name="messageBody" style="height:6em;"></textarea>
                         </li>
                         <li>
                             <br>
