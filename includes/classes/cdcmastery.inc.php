530f50ecafd6165645ce31c8d1251d4f40036c76
Claude Bing <me@claudebing.com>
2015-10-01 15:02:49 -0400

    Add function to get day suffix

--- includes/classes/cdcmastery.inc.php
+++ includes/classes/cdcmastery.inc.php
@@ -123,6 +123,14 @@ class CDCMastery
 		return $this->maxQuestions;
 	}
 
+    public function getOrdinal($number) {
+        $ends = array('th','st','nd','rd','th','th','th','th','th','th');
+        if ((($number % 100) >= 11) && (($number%100) <= 13))
+            return $number. 'th';
+        else
+            return $number. $ends[$number % 10];
+    }
+
     public function getPassingScore(){
         return $this->passingScore;
     }
