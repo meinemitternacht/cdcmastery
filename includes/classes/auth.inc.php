855c1b039adaadd7f4a65250982bf0998fe99233
Claude Bing <me@claudebing.com>
2015-10-03 03:28:04 -0400

    Set the failed login user UUID for log entry

--- includes/classes/auth.inc.php
+++ includes/classes/auth.inc.php
@@ -147,6 +147,7 @@ class auth extends user
 		if(empty($password)){
 			$this->error = "You must provide a password.";
 			$this->log->setAction("ERROR_LOGIN_EMPTY_PASSWORD");
+			$this->log->setUserUUID($this->getUUID());
 			$this->log->saveEntry();
 
 			return false;
@@ -156,7 +157,7 @@ class auth extends user
 
 			if(!isset($_SESSION['rateLimitRecorded'])){
 				$this->log->setAction("ERROR_LOGIN_RATE_LIMIT_REACHED");
-				$this->log->setDetail("User UUID",$this->getUUID());
+				$this->log->setUserUUID($this->getUUID());
 				$this->log->saveEntry();
 				$_SESSION['rateLimitRecorded'] = true;
 			}
@@ -168,7 +169,7 @@ class auth extends user
 			$this->limitLogins(true);
 
 			$this->log->setAction("ERROR_LOGIN_INVALID_PASSWORD");
-			$this->log->setDetail("User UUID",$this->getUUID());
+			$this->log->setUserUUID($this->getUUID());
 			$this->log->saveEntry();
 
 			return false;
@@ -176,7 +177,7 @@ class auth extends user
 		elseif(!$this->getActivationStatus()){
 			$this->error = "Your account has not been activated. Please check your e-mail inbox for the activation e-mail, or contact the support helpdesk for assistance.";
 			$this->log->setAction("ERROR_LOGIN_UNACTIVATED_ACCOUNT");
-			$this->log->setDetail("User UUID",$this->getUUID());
+			$this->log->setUserUUID($this->getUUID());
 			$this->log->saveEntry();
 
 			return false;
@@ -184,7 +185,7 @@ class auth extends user
         elseif($this->getUserDisabled()){
             $this->error = "Your account has been disabled.  If you feel this is in error, open a support ticket.";
             $this->log->setAction("ERROR_LOGIN_USER_DISABLED");
-			$this->log->setDetail("User UUID",$this->getUUID());
+			$this->log->setUserUUID($this->getUUID());
             $this->log->saveEntry();
 
             return false;
