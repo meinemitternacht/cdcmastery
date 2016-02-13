d88ca7f184e7457bcd3bebc813b961da385c607f
Claude Bing <me@claudebing.com>
2016-02-12 22:52:07 -0500

    Track user uuid as google analytics dimension

--- theme/footer.inc.php
+++ theme/footer.inc.php
@@ -88,21 +88,24 @@
 			$('#system-messages-container-block').show();
 		</script>
 		<?php endif; ?>
-		<script type="text/javascript">
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-			var _gaq = _gaq || [];
-			_gaq.push(['_setAccount', 'UA-30696456-1']);
-			_gaq.push(['_setSiteSpeedSampleRate', 100]);
-			_gaq.push(['_trackPageview']);
-			<?php if(isset($_SESSION['userUUID']) && !empty($_SESSION['userUUID'])): ?>ga('set', 'userId', '<?php echo $_SESSION['userUUID']; ?>');<?php endif; ?>
+            <?php
+            // New Google Analytics code to set User ID.
+            // $userId is a unique, persistent, and non-personally identifiable string ID.
+            if (isset($_SESSION['userUUID']) && !empty($_SESSION['userUUID'])) {
+              $gacode = "ga('create', 'UA-30696456-1', 'auto', {'userId': '%s'});";
+              echo sprintf($gacode, $_SESSION['userUUID']);
+            } else {
+              $gacode = "ga('create', 'UA-30696456-1', 'auto');";
+              echo sprintf($gacode);
+            }?>
+            ga('send', 'pageview');
 
-			(function() {
-				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-				<?php if(isset($_SESSION['userUUID']) && !empty($_SESSION['userUUID'])): ?>ga('set', 'userId', '<?php echo $_SESSION['userUUID']; ?>');<?php endif; ?>
-				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			})();
-
-		</script>
+        </script>
 	</body>
 </html>
