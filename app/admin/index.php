21d6f94e365cc2439d08659357cc874d691c121e
Claude Bing <me@claudebing.com>
2016-06-03 06:33:45 -0400

    Restrict some pages to admin

--- app/admin/index.php
+++ app/admin/index.php
@@ -57,8 +57,10 @@ else{
 						<li><a href="/admin/office-symbols" title="Office Symbols"><i class="icon-inline icon-20 ic-office"></i>Office Symbols</a></li>
 						<li><a href="/admin/users" title="User Manager"><i class="icon-inline icon-20 ic-user-multiple"></i>User Manager</a></li>
 						<li><a href="/admin/profile" title="User Profiles"><i class="icon-inline icon-20 ic-user-single"></i>User Profiles</a></li>
+						<?php if($cdcMastery->verifyAdmin()): ?>
                         <li><a href="/admin/activate-users" title="Activate Users"><i class="icon-inline icon-20 ic-relationship"></i>Unactivated Users (<?php echo ($unactivatedUsersCount > 0) ? '<span style="color:red;">'.$unactivatedUsersCount.'</span>' : $unactivatedUsersCount ; ?>)</a></li>
                         <li><a href="/admin/authorize-users" title="Authorize Group for Users"><i class="icon-inline icon-20 ic-relationship"></i>Users Pending Authorization (<?php echo ($authorizationQueueCount > 0) ? '<span style="color:red;">'.$authorizationQueueCount.'</span>' : $authorizationQueueCount ; ?>)</a></li>
+						<?php endif; ?>
 						<li><a href="/admin/users-duplicate" title="View Duplicate Users"><i class="icon-inline icon-20 ic-user-multiple"></i>View Duplicate Users</a></li>
 					</ul>
 				</div>
@@ -74,10 +76,10 @@ else{
 						<li><a href="/admin/log" title="Log"><i class="icon-inline icon-20 ic-log"></i>Log</a></li>
 						<li><a href="/admin/search" title="Search"><i class="icon-inline icon-20 ic-hammer"></i>Search</a></li>
 						<li><a href="/admin/statistics" title="Statistics"><i class="icon-inline icon-20 ic-clipboard"></i>Statistics</a></li>
-						<li><a href="/admin/memcache" title="Memcache Statistics"><i class="icon-inline icon-20 ic-chalkboard"></i>Memcache Statistics</a></li>
 						<li><a href="/admin/bases" title="Base Manager"><i class="icon-inline icon-20 ic-office"></i>Base Manager</a></li>
 						<?php if($cdcMastery->verifyAdmin()): ?>
 						<li><a href="/admin/roles" title="Role Manager"><i class="icon-inline icon-20 ic-user-multiple"></i>Role Manager</a></li>
+						<li><a href="/admin/memcache" title="Memcache Statistics"><i class="icon-inline icon-20 ic-chalkboard"></i>Memcache Statistics</a></li>
 						<?php endif; ?>
 						<li><a href="/admin/upload" title="Upload Files"><i class="icon-inline icon-20 ic-upload"></i>Upload Files</a></li>
 					</ul>
