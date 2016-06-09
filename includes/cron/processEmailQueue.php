8e5468e523f7f788e2715ef896ed6612f34c5553
Claude Bing <me@claudebing.com>
2016-06-08 20:48:31 -0400

    Clean up code and add exit error

--- includes/cron/processEmailQueue.php
+++ includes/cron/processEmailQueue.php
@@ -2,9 +2,9 @@
 define('BASE_PATH', realpath(__DIR__) . "/../..");
 require BASE_PATH . '/includes/bootstrap.inc.php';
 
-if(!$emailQueue->processQueue()){
-	echo "The e-mail queue could not be processed.";
-}
-else{
-	echo "Queue processed successfully.";
+if (!$emailQueue->processQueue()) {
+    echo "The e-mail queue could not be processed.";
+    exit(1);
+} else {
+    echo "Queue processed successfully.";
 }
