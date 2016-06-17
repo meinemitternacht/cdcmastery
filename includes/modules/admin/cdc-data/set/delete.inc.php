fe8ef04e6cb865e5caa1c722466cd48d1ed6efe7
Claude Bing <me@claudebing.com>
2016-06-17 06:12:41 -0400

    Remove volumes/sets, fix statment closing

--- includes/modules/admin/cdc-data/set/delete.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: claude
- * Date: 5/23/15
- * Time: 2:39 AM
- */
--- includes/modules/admin/cdc-data/volume/edit.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: claude
- * Date: 5/22/15
- * Time: 10:01 PM
- */
