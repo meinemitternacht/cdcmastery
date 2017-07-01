e0fdef4b24c4e19aaa841e097b187095ed5fc44a
Claude Bing <me@claudebing.com>
2017-06-30 21:24:21 -0400

    Add RootController

---
+++ src/CDCMastery/Controllers/RootController.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tehbi
+ * Date: 6/30/2017
+ * Time: 8:13 PM
+ */
+
+namespace CDCMastery\Controllers;
+
+
+use CDCMastery\Models\Messages;
+use Monolog\Logger;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class RootController
+{
+    /**
+     * @var ContainerInterface $container
+     */
+    protected $container;
+
+    /**
+     * @var Logger $log
+     */
+    protected $log;
+
+    /**
+     * @var \Twig_Environment
+     */
+    protected $twig;
+
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * RootController constructor.
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+        $this->log = $this->container->get(Logger::class);
+        $this->twig = $this->container->get(\Twig_Environment::class);
+        $this->request = Request::createFromGlobals();
+    }
+
+    /**
+     * @param string $template
+     * @param array $data
+     * @return string
+     */
+    public function render(string $template, array $data = []): string
+    {
+        return $this->twig->render(
+            $template,
+            array_merge(
+                $data,
+                [
+                    'messages' => Messages::get()
+                ]
+            )
+        );
+    }
+}
