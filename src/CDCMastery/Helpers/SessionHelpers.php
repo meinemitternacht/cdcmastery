cdd5342b144f7cef46ce33598b818b7bb0adb06e
Claude Bing <me@claudebing.com>
2017-07-02 22:36:07 -0400

    Add helper functions for admin, supervisor, training manager

--- src/CDCMastery/Helpers/SessionHelpers.php
+++ src/CDCMastery/Helpers/SessionHelpers.php
@@ -11,10 +11,42 @@ namespace CDCMastery\Helpers;
 
 class SessionHelpers
 {
+    const CLASS_ADMIN = 'admin';
+    const CLASS_SUPERVISOR = 'supervisor';
+    const CLASS_TRNG_MGR = 'training_manager';
+
     const KEY_AUTH = 'cdcmastery_auth';
+    const KEY_CLASS = 'user_class';
 
     public static function isLoggedIn(): bool
     {
         return isset($_SESSION[self::KEY_AUTH]);
+    }
+
+    public static function isAdmin(): bool
+    {
+        if (!isset($_SESSION[self::KEY_CLASS])) {
+            return false;
+        }
+
+        return $_SESSION[self::KEY_CLASS] === self::CLASS_ADMIN;
+    }
+
+    public static function isSupervisor(): bool
+    {
+        if (!isset($_SESSION[self::KEY_CLASS])) {
+            return false;
+        }
+
+        return $_SESSION[self::KEY_CLASS] === self::CLASS_SUPERVISOR;
+    }
+
+    public static function isTrainingManager(): bool
+    {
+        if (!isset($_SESSION[self::KEY_CLASS])) {
+            return false;
+        }
+
+        return $_SESSION[self::KEY_CLASS] === self::CLASS_TRNG_MGR;
     }
 }
