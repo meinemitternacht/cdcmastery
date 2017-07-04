42b397d422b4a0c655026532e93e76c00ca84ffe
Claude Bing <me@claudebing.com>
2017-07-04 00:46:27 -0400

    Add DATETIME formats for beginning and end of day

--- src/CDCMastery/Helpers/DateTimeHelpers.php
+++ src/CDCMastery/Helpers/DateTimeHelpers.php
@@ -12,4 +12,6 @@ namespace CDCMastery\Helpers;
 class DateTimeHelpers
 {
     const FMT_DATABASE = 'Y-m-d H:i:s';
+    const FMT_DATABASE_DAY_START = 'Y-m-d 00:00:00';
+    const FMT_DATABASE_DAY_END = 'Y-m-d 23:59:59';
 }
