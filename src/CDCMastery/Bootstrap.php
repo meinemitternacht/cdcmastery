2d082d0003dc5ea9b035c98276772429745f568a
Claude Bing <me@claudebing.com>
2019-11-10 00:51:32 -0500

    Clean up Bootstrap; Fix RuntimeException: Failed to start the session: already started by PHP.

--- src/CDCMastery/Bootstrap.php
+++ src/CDCMastery/Bootstrap.php
@@ -6,9 +6,9 @@
  * Time: 11:12 AM
  */
 
+use DI\Container;
 use DI\ContainerBuilder;
-
-session_start();
+use Monolog\Logger;
 
 define('APP_DIR', realpath(__DIR__));
 define('BASE_DIR', realpath(__DIR__ . '/../..'));
@@ -17,34 +17,28 @@ define('VIEWS_DIR', realpath(__DIR__ . '/Views'));
 
 require VENDOR_DIR . '/autoload.php';
 
-/**
- * @param $severity
- * @param $message
- * @param $file
- * @param $line
- * @throws ErrorException
- */
-function exceptionErrorHandler($severity, $message, $file, $line)
+set_error_handler(static function ($severity, $message, $file, $line)
 {
     if (!(error_reporting() & $severity)) {
         return;
     }
 
     throw new ErrorException($message, 0, $severity, $file, $line);
-}
-
-set_error_handler("exceptionErrorHandler");
-
-$containerBuilder = new ContainerBuilder;
-$containerBuilder->addDefinitions(__DIR__ . '/DIConfig.php');
-$container = $containerBuilder->build();
+});
 
 try{
+    $builder = new ContainerBuilder;
+    $container = $builder->addDefinitions(__DIR__ . '/DIConfig.php')->build();
     $container->get(\mysqli::class);
 } catch (Exception $e) {
-    $log = $container->get(\Monolog\Logger::class);
-
-    $log->alert('site offline :: ' . $e);
+    if ($container instanceof Container) {
+        try {
+            $log = $container->get(Logger::class);
+            $log->alert('site offline :: ' . $e);
+        } catch (Throwable $e) {
+            fwrite(STDERR, 'site offline :: ' . $e);
+        }
+    }
 
     http_response_code(500);
     exit("Site currently offline.  Check back soon!");
