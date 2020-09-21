773ec0fac41abc7ca360a4614d1e1aeeebcf3993
Claude Bing <me@claudebing.com>
2020-09-20 21:53:51 -0400

    cleanup: remove old site files

--- app/auth/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!empty($_SESSION['userUUID'])) {
-	$systemLog->setAction("LOGOUT_SUCCESS");
-	$systemLog->setUserUUID($_SESSION['userUUID']);
-	$systemLog->saveEntry();
-}
-
-session_destroy();
-$cdcMastery->redirect("/");
--- includes/modules/statistics/base/actions.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: tehbi
- * Date: 9/26/2015
- * Time: 8:14 PM
- */
